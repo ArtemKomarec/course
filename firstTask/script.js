e65005613029dc926a89dcd167939f631af68a63
ArtemKomarec <artemkomarec10@gmail.com>
2021-09-27 21:11:09 +0300

    delete class

--- firstTask/script.js
+++ firstTask/script.js
@@ -1,113 +1,56 @@
-class User{
-    constructor(name, surname, patronymic, age) {
-        this.name = name;
-        this.surname = surname;
-        this.patronymic = patronymic;
-        this.age = age;
+calculationAge  = (age) => {
+    return age *=365;
+}
+let nextAge = (age) => {
+    let num = Number(age);
+    return num +=5;
+}
+ gender = () => {
+    let result = confirm("ваш пол женский?")
+    if(result){
+        result = 'женский';
+        return result;
     }
-    get name(){
-        return this._name;
+    else{
+        result = 'мужской';
+        return result;
     }
-    set name(value){
-        if(value.length < 2)
-        {
-            return; // я бы добавил здесь алерт, но только один раз можно использовать
-        }
-        else
-        {
-            this._name = value;
-        }
-    }
-    get surname(){
-        return this._surname;
-    }
-    set surname(value){
-        if(value.length < 4)
-        {
-            return; // я бы добавил здесь алерт, но только один раз можно использовать
-        }
-        else
-        {
-            this._surname = value;
-        }
-    }
-    get patronymic(){
-        return this._patronymic;
-    }
-    set patronymic(value){
-        if(value.length < 4)
-        {
-            return; // я бы добавил здесь алерт, но только один раз можно использовать
-        }
-        else
-        {
-            this._patronymic = value;
-        }
-    }
-    get age(){
-        return this._age;
-    }
-    set age(value){
-        {
-            this._age = value;
-        }
-    }
-    calculationAge  = (age) => {
-        return age *=365;
-    }
-    nextAge  = (age) => {
-        let num = Number(age);
-        return num +=5;
-    }
-    gender = () => {
-        let result = confirm("ваш пол женский?")
-        if(result){
-            result = 'женский';
-            return result;
-        }
-        else{
-            result = 'мужской';
-            return result;
-        }
-    }
-    retired = (age, userGender) => {
-        let result;
-        if(age>56)
-        {
-            switch(userGender){
-                case userGender: 
-                if(age < 66 && userGender == 'мужской')
-                {
-                    result = 'нет';
-                    return result;
-                    break;
-                }
-                else{
-                    result = 'да';
-                    return result;
-                    break;
-                }
-                default:
+}
+retired = (age, userGender) => {
+    let result;
+    if(age>56)
+    {
+        switch(userGender){
+            case userGender: 
+            if(age < 66 && userGender == 'мужской')
+            {
+                result = 'нет';
+                return result;
+                break;
+            }
+            else{
                 result = 'да';
                 return result;
                 break;
             }
-        }
-        else{
-            result = 'нет';
+            default:
+            result = 'да';
             return result;
+            break;
         }
     }
-    print(calcAge, nextAge, userGender, pension){
-        alert("Вашео ФИО:" + surname + " " + name + " " + patronymic + "\n" +
-        "Ваш возраст в годах: " + age +"\n" +
-        "Ваш возраст в днях: " + calcAge + "\n" +
-        "Через 5 лет вам будет: " + nextAge + "\n" +
-        "ваш пол: " + userGender + "\n" +
-        "вы на пенсии? " + pension);
+    else{
+        result = 'нет';
+        return result;
     }
-
-    
+}
+function print(calcAge, nextAge, userGender, pension){
+    alert("Вашео ФИО:" + surname + " " + name + " " + patronymic + "\n" +
+    "Ваш возраст в годах: " + age +"\n" +
+    "Ваш возраст в днях: " + calcAge + "\n" +
+    "Через 5 лет вам будет: " + nextAge + "\n" +
+    "ваш пол: " + userGender + "\n" +
+    "вы на пенсии? " + pension);
 }
 function isString(str){
     for (var i = 0; i < str.length; i++)
@@ -143,9 +86,8 @@ let name = addFcs();
 let surname = addFcs();
 let patronymic = addFcs();
 let age = inputAge();
-let user = new User(name, surname, patronymic, age);
-let calcAge = user.calculationAge(age);
-let nextAge = user.nextAge(age);
-let userGender = user.gender();
-let pension = user.retired(age, userGender);
-user.print(calcAge, nextAge, userGender, pension);
+let calcAge = calculationAge(age);
+nextAge = nextAge(age);
+let userGender = gender();
+let pension = retired(age, userGender);
+print(calcAge, nextAge, userGender, pension);

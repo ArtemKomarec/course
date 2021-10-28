557eb278e396709f829935e018f490dc5802eb53
ArtemKomarec <artemkomarec10@gmail.com>
2021-10-28 17:10:46 +0300

    make figures

---
+++ svg/createSvg.js
@@ -0,0 +1,31 @@
+function makeStar() {
+    const wrapper = document.getElementById('star');
+    const star = document.createElementNS("http://www.w3.org/2000/svg", 'polygon')
+    star.setAttribute('stroke', 'orange');
+    star.setAttribute('fill', 'orange');
+    star.setAttribute('stroke-width', '5')
+    star.setAttribute('points', '70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41');
+    wrapper.appendChild(star);
+}
+
+function makeTriangle() {
+    const wrapper = document.getElementById('triangle');
+    const triangle = document.createElementNS("http://www.w3.org/2000/svg", 'polygon')
+    triangle.setAttribute('stroke', 'green');
+    triangle.setAttribute('fill', 'green');
+    triangle.setAttribute('points', '5,135 115,5 225,135');
+    wrapper.appendChild(triangle);
+}
+
+function makeCircle() {
+    const wrapper = document.getElementById('circle');
+    const circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle')
+    circle.setAttribute('stroke', 'red');
+    circle.setAttribute('stroke-width', '5')
+    circle.setAttribute('fill', 'purple');
+    circle.setAttribute('r', '65');
+    circle.setAttribute('cx', '70');
+    circle.setAttribute('cy', '70');
+    circle.setAttribute('stroke-dasharray', '20,10,40,10')
+    wrapper.appendChild(circle);
+}
